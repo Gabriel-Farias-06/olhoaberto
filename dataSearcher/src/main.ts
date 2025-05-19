46f72f0eb2b8a169bcf6e607414fee9d7f1a1e14
Gabriel-Farias-06 <gabriel.rfarias@a.ucb.br>
2025-05-19 13:36:35 -0300

    create: update and delete users, and sessions

--- dataSearcher/src/main.ts
+++ dataSearcher/src/main.ts
@@ -1,18 +1,35 @@
 import dotenv from "dotenv";
 import express from "express";
+import session from "express-session";
+
 import {
   loginController,
   signUpController,
   streamArticles,
   updateInstructions,
+  authenticatedMiddlewareController,
+  deleteUserController,
+  updateUserController,
 } from "./controllers";
 import { connectDb } from "./infra/db";
+
+
 
 dotenv.config();
 
 connectDb().then(async () => {
   const app = express();
   app.use(express.json());
+
+  app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 30 * 60 * 1000
+    }
+  }));
 
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -21,17 +38,18 @@ connectDb().then(async () => {
       "GET, POST, PUT, DELETE, OPTIONS",
     );
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
   });
 
   app.get("/stream", async (req, res) => {
     const query = req.query.q as string;
-    const email = req.query.email as string;
+    const email = req.query.email as string | undefined;
+
     console.log({ query });
 
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     res.setHeader("Transfer-Encoding", "chunked");
-    res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Cache-Control", "no-cache");
 
     try {
@@ -48,18 +66,32 @@ connectDb().then(async () => {
   app.post("/signup", async (req, res) => {
     const { name, email, password } = req.body;
 
-    signUpController(name, email, password, res);
+    signUpController(name, email, password, req, res);
   });
 
   app.post("/login", async (req, res) => {
     const { email, password } = req.body;
-    loginController(email, password, res);
+    loginController(email, password, req, res);
   });
 
-  app.put("/instructions", async (req, res) => {
-    const { email, password, instructions } = req.body;
+  app.post('/logout', async (req, res) => {
+    req.session.destroy(() => {
+      res.status(200).json({ message: 'Logout sucessful' })
+    })
+  })
 
-    updateInstructions(email, password, instructions, res);
+  app.put('/updateUser', authenticatedMiddlewareController, async (req, res) => {
+    updateUserController(req, res);
+  }
+  app.delete('/deleteUser', authenticatedMiddlewareController, async (req, res) => {
+    deleteUserController(req, res);
+  })
+  
+
+  app.put("/instructions", authenticatedMiddlewareController, async (req, res) => {
+    const { instructions } = req.body;
+    const { email } = req.session.user;
+    updateInstructions(email, instructions, res);
   });
 
   app.listen(4000, () => {
