eff6fb0a594cebb161c69330214a68dae85b0c27
feitosadavi <davifeitosa.dev@protonmail.com>
2025-06-17 19:22:05 -0300

    chore: path alias build config

--- olhoabertofrontend/next.config.ts
+++ olhoabertofrontend/next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  compiler: {
-    styledComponents: true
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
-  
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
